5ed2077e68a0c0800825511e02518155de50aae7
Daniel Boros <dancixx@gmail.com>
2024-02-07 02:21:42 +0100

    refactor: lstm model

--- fou-lstm/src/main.rs
+++ fou-lstm/src/main.rs
@@ -1,73 +1,86 @@
 use candle_core::{DType, Device, Module, Result, Tensor};
+use candle_datasets::Batcher;
 use candle_nn::{
-    linear, lstm, AdamW, LSTMConfig, Linear, Optimizer, ParamsAdamW, VarBuilder, VarMap, LSTM, RNN,
+    linear, AdamW, LSTMConfig, Linear, Optimizer, ParamsAdamW, VarBuilder, VarMap, LSTM, RNN,
 };
+use stochastic_rs::diffusions::ou::fou;
 
 struct Model {
-    num_layers: usize,
-    lstm1: LSTM,
-    linear1: Linear,
+    // num_layers: usize,
+    lstm: Vec<LSTM>,
+    out_layer: Linear,
 }
 
 impl Model {
     #[must_use]
     fn new(
         vs: VarBuilder,
-        input_dim: usize,
-        hidden_size: usize,
-        num_layers: usize,
+        in_dim: usize,
+        hidden_dim: usize,
+        num_lstm_layers: usize,
         out_dim: usize,
     ) -> Result<Self> {
-        let lstm1 = lstm(
-            input_dim,
-            hidden_size,
-            LSTMConfig::default(),
-            vs.push_prefix("lstm1"),
-        )?;
-        let linear1 = linear(hidden_size, out_dim, vs.push_prefix("linear1"))?;
-
+        let vs = &vs.pp("lstm");
+        let mut lstm_layers = Vec::with_capacity(num_lstm_layers);
+        for layer_idx in 0..num_lstm_layers {
+            let config = LSTMConfig {
+                layer_idx,
+                ..Default::default()
+            };
+            let lstm = candle_nn::lstm(in_dim, hidden_dim, config, vs.clone())?;
+            lstm_layers.push(lstm);
+        }
+        let out_layer = linear(hidden_dim, out_dim, vs.clone())?;
         Ok(Self {
-            lstm1,
-            linear1,
-            num_layers,
+            lstm: lstm_layers,
+            out_layer,
         })
     }
 
     fn forward(&self, x: &Tensor) -> Result<Tensor> {
-        let state = self
-            .lstm1
-            .step(&x, &self.lstm1.zero_state(self.num_layers)?)?;
-
-        let out = self.lstm1.states_to_tensor(&[state])?;
-        let out = self.linear1.forward(&out)?;
-
+        let mut x = x.clone();
+        for layer in &self.lstm {
+            let states = layer.seq(&x)?;
+            x = layer.states_to_tensor(&states)?.unsqueeze(0)?;
+        }
+        let out = self.out_layer.forward(&x)?;
         Ok(out)
     }
 }
 
 fn main() -> anyhow::Result<()> {
     let device = Device::cuda_if_available(0).unwrap_or(Device::Cpu);
-    let mut varmap = VarMap::new();
-    let vs = VarBuilder::from_varmap(&varmap, DType::F32, &device);
+    let varmap = VarMap::new();
+    let vs = VarBuilder::from_varmap(&varmap, DType::F64, &device);
 
-    let input_dim = 1_604_usize;
-    let hidden_size = 64_usize;
-    let num_layers = 2_usize;
+    let epochs = 25;
+    let epoch_size = 128;
+    let in_dim = 1_600_usize;
+    let hidden_dim = 64_usize;
+    let num_lstm_layers = 2_usize;
     let out_dim = 1_usize;
-    let net = Model::new(vs, input_dim, hidden_size, num_layers, out_dim).unwrap();
+    let batch_size = 64_usize;
+    let net = Model::new(vs, in_dim, hidden_dim, num_lstm_layers, out_dim).unwrap();
     let adamw_params = ParamsAdamW::default();
     let mut opt = AdamW::new(varmap.all_vars(), adamw_params)?;
 
-    let epochs = 25;
-
     for _ in 0..epochs {
-        let x = Tensor::randn(0_f32, 1.0_f32, &[num_layers, input_dim], &device)?;
-        println!("x: {:?}", x);
-        let y = net.forward(&x)?;
-        let loss = y.mean(0)?;
-        opt.backward_step(&loss)?;
+        let mut paths = Vec::with_capacity(epoch_size);
+        for _ in 0..epoch_size {
+            let path_i = fou(0.7, 2.8, 1.0, 5.0, in_dim, Some(0.0), Some(1.0));
+            paths.push(Ok(Tensor::from_vec(path_i, &[in_dim], &device)?));
+        }
+        let batcher = Batcher::new_r1(paths.into_iter())
+            .batch_size(batch_size)
+            .return_last_incomplete_batch(true);
 
-        println!("loss: {:?}", loss);
+        for batch in batcher {
+            let input = batch?;
+            let y = net.forward(&input.unsqueeze(0)?)?;
+            let loss = y.mean(0)?;
+            opt.backward_step(&loss)?;
+            println!("loss: {:?}", loss);
+        }
     }
 
     Ok(())
